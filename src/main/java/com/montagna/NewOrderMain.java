8e57dce8e8256f26fe8f56663c488ae298e62299
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-11-04 00:32:56 -0300

    Adding New Topic, New Producer and Consumer

--- src/main/java/com/montagna/NewOrderMain.java
+++ src/main/java/com/montagna/NewOrderMain.java
@@ -1,5 +1,6 @@
 package com.montagna;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,24 +15,37 @@ public class NewOrderMain {
 
         // Criar um Produtor do Kafka
         var kafkaProducer = new KafkaProducer<String, String>(properties());
+
+        // Valores que serão atribuídos a key e ao value
         var value = "ValorKafka1,456456,464567";
+        var emailValue = "EMAIL, Assunto, Enviado";
+
+        // Cria as mensagens que serão enviadas ao topic
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 
-        // Enviar para o tópico do Kafka
-        kafkaProducer.send(record,(data, ex) -> {
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", emailValue, emailValue);
+
+
+        // Trata o retorno da mensagem
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("Sucesso Enviando " + data.topic() + ":::partition " + data.partition() + "/ offset" + "/" + data.timestamp());
-        }).get();
-//        kafkaProducer.close();
-//        kafkaProducer.flush();
+        };
+
+        // Enviar para o tópico do Kafka
+        kafkaProducer.send(record, callback).get();
+        kafkaProducer.send(emailRecord, callback).get();
+
+        //        kafkaProducer.close();
+        //        kafkaProducer.flush();
     }
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "henrique-virtualbox:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "PLAINTEXT://henrique-virtualbox:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
