d83deb1f19ebedc9822aff659de5bf3f1571cd5f
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-11-08 23:57:40 -0300

    Send 100 messages to topics and adding UUID to parallel processing using partitions

--- src/main/java/com/montagna/NewOrderMain.java
+++ src/main/java/com/montagna/NewOrderMain.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
@@ -16,31 +17,38 @@ public class NewOrderMain {
         // Criar um Produtor do Kafka
         var kafkaProducer = new KafkaProducer<String, String>(properties());
 
-        // Valores que serão atribuídos a key e ao value
-        var value = "ValorKafka1,456456,464567";
-        var emailValue = "EMAIL, Assunto, Enviado";
+        for (var i = 0; i <= 100; i++) {
 
-        // Cria as mensagens que serão enviadas ao topic
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+            // Valores que serão atribuídos a key e ao value
+            var key = UUID.randomUUID().toString();
+            var value = String.format("ValorKafka, %a, %a", Math.random(), Math.random());
+            var emailValue = "EMAIL, Assunto, Enviado";
 
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", emailValue, emailValue);
+            // Cria as mensagens que serão enviadas ao topic
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, emailValue);
 
 
-        // Trata o retorno da mensagem
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("Sucesso Enviando " + data.topic() + ":::partition " + data.partition() + "/ offset" + "/" + data.timestamp());
-        };
+            // Trata o retorno da mensagem
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("Sucesso Enviando " + data.topic() + ":::partition " + data.partition() + "/ offset" + "/" + data.timestamp());
+            };
 
-        // Enviar para o tópico do Kafka
-        kafkaProducer.send(record, callback).get();
-        kafkaProducer.send(emailRecord, callback).get();
+            // Enviar para o tópico do Kafka
+            kafkaProducer.send(record, callback).get();
+            kafkaProducer.send(emailRecord, callback).get();
 
-        //        kafkaProducer.close();
-        //        kafkaProducer.flush();
+            //        kafkaProducer.close();
+            //        kafkaProducer.flush();
+
+        }
+
+
     }
 
     private static Properties properties() {
