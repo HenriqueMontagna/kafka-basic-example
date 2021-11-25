9efbe9ba83de196bfa89d10b94255f263584adef
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-11-25 00:17:16 -0300

    Changing ConsumerFunction.java to accept any class

--- src/main/java/com/montagna/ConsumerFunction.java
+++
@@ -1,9 +0,0 @@
-package com.montagna;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction {
-
-    void consume(ConsumerRecord<String, String> record);
-
-}
